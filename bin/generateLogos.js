158a5bd0be9267a1939c30a62b7cdfa12cacc954
Melvyn Kim <melvynkim@gmail.com>
2023-04-06 02:37:41 -0700

    feat: generate more logos

--- bin/generateLogos.js
+++ bin/generateLogos.js
@@ -2,6 +2,27 @@ const axios = require('axios');
 const fs = require('fs');
 const faker = require('faker');
 
+const categories = [
+  "Search",
+  "Ads",
+  "Cloud",
+  "Retail",
+  "Media",
+  "Hardware",
+  "Software",
+  "Services",
+  "Social",
+  "Privacy",
+  "Open Source",
+  "Automotive",
+  "Energy",
+  "Transportation",
+  "Travel",
+  "Hospitality",
+  "Collaboration",
+  "Enterprise"
+];
+
 const getRandomCategories = () => {
   const numCategories = Math.floor(Math.random() * 3) + 1;
   const shuffledCategories = faker.helpers.shuffle(categories);
@@ -14,8 +35,8 @@ const getRandomLogos = async () => {
   for (let i = 0; i < numLogos; i++) {
     const company = await axios.get(`https://autocomplete.clearbit.com/v1/companies/suggest?query=${Math.random().toString(36).substring(7)}`);
     const image = `https://logo.clearbit.com/${company.data[0].domain}`;
-    const title = company.data[0].name;
-    const subtitle = company.data[0].tags[0].name;
+    const title = faker.company.companyName();
+    const subtitle = faker.lorem.words();
     const categories = getRandomCategories();
     logos.push({ image, title, subtitle, categories });
   }
