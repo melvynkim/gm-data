4d0dbd70f6631d92cd892206fd2b713a029d79b2
Melvyn Kim <melvynkim@gmail.com>
2023-04-06 02:30:10 -0700

    feat: logo generator

---
+++ bin/generateLogos.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const fs = require('fs');
+const faker = require('faker');
+
+const getRandomCategories = () => {
+  const numCategories = Math.floor(Math.random() * 3) + 1;
+  const shuffledCategories = faker.helpers.shuffle(categories);
+  return shuffledCategories.slice(0, numCategories);
+};
+
+const getRandomLogos = async () => {
+  const numLogos = Math.floor(Math.random() * 181) + 20;
+  const logos = [];
+  for (let i = 0; i < numLogos; i++) {
+    const company = await axios.get(`https://autocomplete.clearbit.com/v1/companies/suggest?query=${Math.random().toString(36).substring(7)}`);
+    const image = `https://logo.clearbit.com/${company.data[0].domain}`;
+    const title = company.data[0].name;
+    const subtitle = company.data[0].tags[0].name;
+    const categories = getRandomCategories();
+    logos.push({ image, title, subtitle, categories });
+  }
+  return logos;
+};
+
+const generateLogosFile = async () => {
+  const logos = await getRandomLogos();
+  fs.writeFile('logos.json', JSON.stringify(logos, null, 2), err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('File has been created');
+  });
+};
+
+generateLogosFile();
